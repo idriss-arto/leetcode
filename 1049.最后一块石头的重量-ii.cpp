464f90c51c9425dd28ae21693416f859caafc0ce
AFU <294305080@qq.com>
2025-04-02 20:45:10 +0800

    25.04.02-动态规划

---
+++ 1049.最后一块石头的重量-ii.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=1049 lang=cpp
+ * 动态规划
+ * [1049] 最后一块石头的重量 II
+ */
+
+/*
+ * 思路
+ ! 本题其实是尽量让石头分成重量相同的两堆（尽可能相同），相撞之后剩下的石头就是最小的。
+ * 一堆的石头重量是sum，那么我们就尽可能拼成重量为sum / 2 的石头堆。 
+ * 这样剩下的石头堆也是尽可能接近 sum/2 的重量。 
+ * 那么此时问题就是有一堆石头，每个石头都有自己的重量，是否可以装满最大重量为sum / 2的背包。
+*/
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int sum = 0;
+        for (int& i : stones) {
+            sum += i; 
+        }
+        int target = sum / 2;
+        
+        /*
+         * 二维dp数组版本
+        vector<vector<int>> dp(stones.size(), vector<int>(target + 1, 0));
+        for (int j = stones[0]; j <= target; j++) {
+            dp[0][j] = stones[0];
+        }
+        for (int i = 1; i < stones.size(); i++) {
+            for (int j = 1; j <= target; j++) {
+                if (j < stones[i]) {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j - stones[i]] + stones[i]);
+                }
+            }
+        }
+
+        return sum - dp[stones.size() - 1][target] * 2;
+        */
+
+        //* 滚动dp数组写法
+        vector<int> dp(target+1, 0);
+        for (int i = 0; i < stones.size(); i++) {
+            for (int j = target; j >= stones[i]; j--) {
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
+            }
+        }
+        
+        return sum - dp[target] * 2;
+    }
+};
+// @lc code=end
+
---
+++ 494.目标和.cpp
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode.cn id=494 lang=cpp
+ * 动态规划
+ * [494] 目标和
+ */
+
+/*
+ * 我的思路：
+ * 让dp[i][j]表示前i个数组成数字j的可能方式数
+ * 则状态转移方程为：dp[i][j] = dp[i-1][j-nums[i]] + dp[i-1][j+nums[i]]
+ * 问题：target范围为[-1000,1000]，所以得让dp[i][j]表示前i个数组成数字（j-1000）的可能方式数
+ ? 理解难度低，但计算较为复杂
+ * 
+ * 题解思路：
+ * 假设nums中数字全部取正时，和为sum
+ * 为了取得target，可以将取正的数全部挪到左边，取负的数全部挪到右边
+ * 即取正的数的和为left，取负的数的绝对值和为right
+ * 则此时有left+right=sum，left-right=target
+ * 则2*left = target+sum，其中target和sum是已知的，可以求出left
+ ! 注意：target+sum为奇数时，left不为整数，即无解
+ * 则原问题转换为求“取nums中的数（每一个数分为取和不取），使得和为left的取法计数”
+*/
+ 
+// @lc code=start
+#include <vector>
+#include <math.h>
+using namespace std;
+/*
+ * 我的解法
+ * 思路：
+ * 让dp[i][j]表示前i个数组成数字j的可能方式数
+ * 则状态转移方程为：dp[i][j] = dp[i-1][j-nums[i]] + dp[i-1][j+nums[i]]
+ * 问题：target范围为[-1000,1000]，所以让dp[i][j]表示前i个数组成数字（j-1000）的可能方式数
+*/
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        /*
+         * 因为target范围为[-1000,1000]，所以让dp[i][j]表示前i个数组成数字（j-1000）的可能方式数
+         * 则状态转移方程为：dp[i][j] = dp[i-1][j-nums[i]] + dp[i-1][j+nums[i]]
+        */
+        vector<vector<int>> dp(nums.size(), vector<int>(2005, 0));
+        dp[0][1000+nums[0]] += 1;
+        dp[0][1000-nums[0]] += 1;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j <= 2000; j++) {
+                if (j - nums[i] >= 0) dp[i][j] += dp[i-1][j-nums[i]];
+                if (j + nums[i] <= 2000) dp[i][j] += dp[i-1][j+nums[i]];
+            }
+        }
+        return dp[nums.size() - 1][target + 1000];
+    }
+};
+
+/*
+ * 题解解法
+ * 思路：
+ * 假设nums中数字全部取正时，和为sum
+ * 为了取得target，可以将取正的数全部挪到左边，取负的数全部挪到右边
+ * 即取正的数的和为left，取负的数的绝对值和为right
+ * 则此时有left+right=sum，left-right=target
+ * 则2*left = target+sum，其中target和sum是已知的，可以求出left
+ ! 注意：target+sum为奇数时，left不为整数，即无解
+ * 则原问题转换为求“取nums中的数（每一个数分为取和不取），使得和为left的取法计数”
+*/
+class Solution2 {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) sum += nums[i];
+        if (abs(target) > sum) return 0;        //* 此时没有方案
+        if ((target + sum) % 2 == 1) return 0;  //! 此时没有方案
+        int bagSize = (target + sum) / 2;
+        
+        vector<vector<int>> dp(nums.size(), vector<int>(bagSize + 1, 0));
+        
+        //* 初始化最上行
+        if (nums[0] <= bagSize) dp[0][nums[0]] = 1; 
+
+        //* 初始化最左列，最左列其他数值在递推公式中就完成了赋值
+        dp[0][0] = 1; 
+
+        int numZero = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) numZero++;
+            dp[i][0] = (int) pow(2.0, numZero);
+        }
+
+        //* 以下遍历顺序行列可以颠倒
+        for (int i = 1; i < nums.size(); i++) { 
+            for (int j = 0; j <= bagSize; j++) {
+                if (nums[i] > j) dp[i][j] = dp[i - 1][j]; 
+                else dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i]];
+            }
+        }
+        return dp[nums.size() - 1][bagSize];
+    }
+};
+// @lc code=end
+
