2ea82c8ed517bed3efefea6d445b2a665989b3b8
AFU <294305080@qq.com>
2025-05-12 11:37:34 +0800

    25.05.12-每日练习（动态规划）

--- 2874.有序三元组中的最大值-ii.cpp
+++ 2874.有序三元组中的最大值-ii.cpp
@@ -1,42 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=2874 lang=cpp
+ * 动态规划
+ * [2874] 有序三元组中的最大值 II
+ */
+
+/*
+ * 固定j，找[0, j-1)的最大值和[j+1, nums.size())的最大值
+*/
+
 // @lc code=start
-#include <iostream>
 #include <vector>
-#include <stdio.h>
 using namespace std;
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
         vector<int> leftMaxIndex(nums.size(), 0);
         for (int i = 1; i < nums.size(); i++) {
-            if (nums[leftMaxIndex[i-1]] > nums[i]) leftMaxIndex[i] = leftMaxIndex[i-1];
-            else leftMaxIndex[i] = i;
+            if (nums[leftMaxIndex[i-1]] > nums[i-1]) leftMaxIndex[i] = leftMaxIndex[i-1];
+            else leftMaxIndex[i] = i-1;
         }
 
         vector<int> rightMaxIndex(nums.size(), nums.size()-1);
         for (int i = nums.size() - 2; i >= 0; i--) {
             if (nums[rightMaxIndex[i+1]] > nums[i+1]) rightMaxIndex[i] = rightMaxIndex[i+1];
-            else rightMaxIndex[i] = i + 1;
+            else rightMaxIndex[i] = i+1;
         }
 
         long long result = 0;
         for (int i = 1; i <= nums.size() - 2; i++) {
-            if (rightMaxIndex[i] == i) continue;
             long long tmp = (long long)(nums[leftMaxIndex[i]] - nums[i]) * nums[rightMaxIndex[i]];
-            if (tmp > result) {
-                result = tmp;
-                printf("%d %d %d", leftMaxIndex[i], i, rightMaxIndex[i]);
-            }
+            result = max(result, tmp);
         }
 
         return result;
     }
 };
-
-int main() {
-    Solution a;
-    vector<int> nums = {10, 13, 6, 2};
-    a.maximumTripletValue(nums);
-    return 0;
-}
 // @lc code=end
 
