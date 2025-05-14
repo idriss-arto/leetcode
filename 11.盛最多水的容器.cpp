a0bb290cc2805d60e8b89ad3ae141e948ef6e3f2
AFU <294305080@qq.com>
2025-05-14 14:34:30 +0800

    25.05.14-每日练习（hot100-双指针）

---
+++ 11.盛最多水的容器.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=cpp
+ * 双指针
+ ! [11] 盛最多水的容器
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+
+/*
+ * 官方题解，思路：
+ * 双指针代表的是 可以作为容器边界的所有位置的范围。
+ * 在一开始，双指针指向数组的左右边界，表示 数组中所有的位置都可以作为容器的边界，
+ * 因为我们还没有进行过任何尝试。
+ * 在这之后，我们每次将 对应的数字较小的那个指针 往 另一个指针 的方向移动一个位置，
+ * 就表示我们认为 这个指针不可能再作为容器的边界了。
+ * 
+ * 为什么移动对应数字较小的那个指针呢？
+ ! 这是因为，容纳的水量是由 两个指针指向的数字中较小值∗指针之间的距离 决定的。
+ * 如果我们移动对应数字较大的那个指针，
+ * 那么前者「两个指针指向的数字中较小值」不会增加，后者「指针之间的距离」会减小，那么这个乘积会减小。
+ * 因此，我们移动对应数字较大的那个指针是不合理的。因此，我们移动对应数字较小的那个指针。
+*/
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0, r = height.size() - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = min(height[l], height[r]) * (r - l);
+            ans = max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+        }
+        return ans;
+    }
+};
+
+//* 我的思路，记录左侧最高的板子
+//! 错了！！当前板子和左侧最高不一定构成最大容器，例如[1,2,1]
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int leftMaxIndex = 0, right = 1, result = 0;
+
+        while (right < height.size()) {
+            result = max(result, min(height[leftMaxIndex], height[right]) * (right - leftMaxIndex));
+            if (height[right] > height[leftMaxIndex]) leftMaxIndex = right;
+            right++;
+        }
+
+        return result;
+    }
+};
+// @lc code=end
+
--- 283.移动零.cpp
+++ 283.移动零.cpp
@@ -4,6 +4,14 @@
  * [283] 移动零
  */
 
+/*
+ * 思路：
+ * 使用双指针，左指针指向当前已经处理好的序列的尾部，右指针指向待处理序列的头部。
+ * 右指针不断向右移动，每次右指针指向非零数，则将左右指针对应的数交换，同时左指针右移。
+ * 显然，遇到0之前，left和right指向同一元素，即自己与自己交换
+ * 遇到第一个0之后，left左边是已经处理好的序列，left指向第一个0
+*/
+
 // @lc code=start
 #include <vector>
 using namespace std;
@@ -11,16 +19,9 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int slow = 0, fast = 0;
-        for (; fast < nums.size(); fast++)
-        {
-            if (nums[fast] != 0)
-            {
-                nums[slow] = nums[fast];
-                if (slow != fast)
-                //* 注意不要错误置零，比如[1]，[2,0]
-                {
-                    nums[fast] = 0;
-                }
+        for (; fast < nums.size(); fast++) {
+            if (nums[fast] != 0) {
+                swap(nums[slow], nums[fast]);
                 slow++;
             }
         }
