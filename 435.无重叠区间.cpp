731efaf2821433022ab03471eaad304c25097fbc
AFU <294305080@qq.com>
2025-05-15 10:38:40 +0800

    25.05.15-复习（贪心）

--- 435.无重叠区间.cpp
+++ 435.无重叠区间.cpp
@@ -1,10 +1,26 @@
 /*
  * @lc app=leetcode.cn id=435 lang=cpp
  * 贪心，重叠区间
- * [435] 无重叠区间
+ ! [435] 无重叠区间
  */
 
-//* 和452殊途同归
+/*
+ * 思路：
+ * 可以以左边界递增排序，也可以以右边界递增排序，不影响以下两种方案
+ * 一：
+ * count表示需要删除的区间数量，初始化为0。
+ * 比较新来的区域起点和当前重叠区间终点，
+ * 如果有重合，则count++，重置重叠区间终点，
+ * 无重合，则重叠区间设为新来区间。
+ * 最后返回count。
+ * 
+ * 二：
+ * count表示最后留下的无重叠区间数量，初始化为1。
+ * 比较新来的区域起点和当前重叠区间终点，
+ * 如果有重合，则重置重叠区间终点，
+ * 无重合，则count++，重叠区间设为新来区间。
+ * 最后返回intervals.size() - count。
+*/
 
 // @lc code=start
 #include <vector>
@@ -22,12 +38,17 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if (intervals.size() == 0) return 0;
         sort(intervals.begin(), intervals.end(), cmp);
-        int count = 1;  //* 记录非交叉区间的个数
+        int count = 1;              //* 记录非交叉区间的个数
         int end = intervals[0][1];  //* 记录区间分割点
         for (int i = 1; i < intervals.size(); i++) {
+            //* 无重叠
             if (intervals[i][0] >= end) {
                 end = intervals[i][1];
                 count++;
+            }
+            //* 有重叠
+            else {
+                //* 这里遇到重叠的不更新右边界是因为用的右边界排序，一开始的右边界确定是最小的
             }
         }
         //! 注意这里
@@ -35,7 +56,7 @@ public:
     }
 };
 
-class Solution2 {
+class Solution {
 public:
     static bool cmp (const vector<int>& a, const vector<int>& b) {
         return a[0] < b[0];     //* 改为左边界排序
@@ -43,11 +64,13 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if (intervals.size() == 0) return 0;
         sort(intervals.begin(), intervals.end(), cmp);
-        int count = 0;  //* 注意这里从0开始，因为是记录重叠区间
+        int count = 0;              //* 注意这里从0开始，因为是记录重叠区间
         int end = intervals[0][1];  //* 记录区间分割点
-        for (int i = 1; i < intervals.size(); i++) {   
-            if (intervals[i][0] >= end)  end = intervals[i][1];     //* 无重叠的情况
-            else {  //* 重叠情况 
+        for (int i = 1; i < intervals.size(); i++) {
+            //* 无重叠的情况
+            if (intervals[i][0] >= end)  end = intervals[i][1];     
+            //* 重叠情况
+            else {                   
                 end = min(end, intervals[i][1]);
                 count++;
             }
--- 763.划分字母区间.cpp
+++ 763.划分字母区间.cpp
@@ -1,8 +1,23 @@
 /*
  * @lc app=leetcode.cn id=763 lang=cpp
  * 贪心，区间
- * [763] 划分字母区间
+ ! [763] 划分字母区间
  */
+
+/*
+ * 思路：
+ * 题目要求同一字母最多出现在一个片段中，所以需要统计每个字母的出现边界。
+ * 在遍历的过程中相当于是要找每一个字母的边界，
+ * 如果找到之前遍历过的所有字母的最远边界，说明这个边界就是分割点了。
+ * 即此时前面出现过所有字母，最远也就到这个边界了。
+ * 
+ * 可以分为如下两步：
+ * 1.统计每一个字符最后出现的位置
+ * 2.从头遍历字符，并更新字符的最远出现下标，如果找到字符最远出现位置下标和当前下标相等了，则找到了分割点
+ * 
+ * 
+ * 还有个思路是统计每个字母的左边界和右边界，然后做合并区间（即重叠区间判定+对应操作），但很麻烦
+*/
 
 // @lc code=start
 #include <vector>
@@ -11,15 +26,17 @@ using namespace std;
 class Solution {
 public:
     vector<int> partitionLabels(string S) {
-        int hash[27] = {0};     //* i为字符，hash[i]为字符出现的最后位置
+        int rightBorder[27] = {0};              //* i为字符，rightBorder[i]为字符出现的最后位置
         for (int i = 0; i < S.size(); i++) {    //* 统计每一个字符最后出现的位置
-            hash[S[i] - 'a'] = i;
+            rightBorder[S[i] - 'a'] = i;
         }
+
         vector<int> result;
         int left = 0;
         int right = 0;
+
         for (int i = 0; i < S.size(); i++) {
-            right = max(right, hash[S[i] - 'a']);   //* 找到字符出现的最远边界
+            right = max(right, rightBorder[S[i] - 'a']);   //* 找到字符出现的最远边界
             if (i == right) {
                 result.push_back(right - left + 1);
                 left = i + 1;
