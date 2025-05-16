35058b0546497a87a5efa55318592d5af62e361e
AFU <294305080@qq.com>
2025-05-16 16:33:22 +0800

    25.05.16-每日练习（hot100-子串）

---
+++ 560.和为-k-的子数组.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=560 lang=cpp
+ * 前缀和 + 哈希表优化
+ ! [560] 和为 K 的子数组
+ */
+
+// @lc code=start
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+/*
+ * 我的思路：前缀和
+*/
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        vector<int> sum(nums.size() + 1, 0);
+        sum[1] = nums[0];
+        for (int i = 2; i <= nums.size(); i++) {
+            sum[i] = sum[i-1] + nums[i - 1];
+        }
+
+        int result = 0;
+        for (int i = 0; i <= nums.size(); i++) {
+            for (int j = i + 1; j <= nums.size(); j++) {
+                if ((sum[j] - sum[i]) == k) result++;
+            }
+        }
+
+        return result;
+    }
+};
+
+/*
+ * 官方题解，思路：
+ * 前缀和 + 哈希表优化
+ * 在考虑以 i 结尾的和为 k 的连续子数组个数时，
+ * 只要统计目前有多少个前缀和为 preSum[i]−k 的 preSum[j] （0 <= j < i）即可
+*/
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int count = 0, preSum = 0;
+        for (auto& x : nums) {
+            preSum += x;
+            //* 从map中寻找满足preSum[i]-preSum[j]==k的j有几个
+            if (mp.find(preSum - k) != mp.end()) {
+                count += mp[preSum - k];
+            }
+            mp[preSum]++;
+        }
+        return count;
+    }
+};
+// @lc code=end
+
--- 76.最小覆盖子串.cpp
+++ 76.最小覆盖子串.cpp
@@ -1,25 +1,28 @@
 /*
  * @lc app=leetcode.cn id=76 lang=cpp
  * 滑动窗口
- * [76] 最小覆盖子串
+ ! [76] 最小覆盖子串
  */
 
 // @lc code=start
 #include<string>
 #include <unordered_map>
 using namespace std;
+
+/*
+ * 思路：
+ * 用unordered_map记录两个字符串中出现字符数量
+ * right先往右走，直到满足s从left到right的字符能覆盖t，
+ * 再尝试left往右走，缩减s子串长度
+*/
 class Solution {
 public:
-    // 多个函数要用的参数最好不在函数内定义，否则传参很花时间
+    //* 多个函数要用的参数最好不在函数内定义，否则传参很花时间
     unordered_map<char, int> cnt1, cnt2;
     
-    bool isOK()
-    {
-        auto it = cnt2.begin();
-        while(it != cnt2.end())
-        {
-            if(cnt1[it->first] < it->second)
-            {
+    bool isOK() {
+        for (auto it = cnt2.begin(); it != cnt2.end(); it++) {
+            if(cnt1[it->first] < it->second) {
                 return false;
             }
             it++;
@@ -39,58 +42,62 @@ public:
         int result = INT_MAX;
         int border[2] = {0};
 
-        for(int i = 0; i < t.length(); i++)
-        {
+        for (int i = 0; i < t.length(); i++) {
             ++cnt2[t[i]];
         }
 
         int left = 0;
-        for (int right = 0; right < s.length(); ++right) 
-        {
+        for (int right = 0; right < s.length(); ++right) {
             ++cnt1[s[right]];
-            while (isOK() && left <= right)
-            {
+            while (isOK() && left <= right) {
                 int subL = right - left + 1;
-                if(result > subL)
-                {
+                if (result > subL) {
                     result = subL;
                     border[0] = left;
                     border[1] = right;
                 }
                 auto it = cnt1.find(s[left]);
                 --it->second;
+                //* 或者 cnt1[s[left]]--;
                 ++left;
             }
         }
 
-        if(result != INT_MAX)
-        {
+        if (result != INT_MAX) {
             return s.substr(border[0], result);
         }
-        else
-        {
+        else {
             return "";
         }
     }
+};
 
-    /*
+/*
+ * 思路：
+ * 添加和删除不是看子串是否覆盖t，而是看指针所指向字母的个数是否大于所需
+ * 添加时，right指向的字符小于所需才count++，count==t.size()时说明覆盖了
+ * 删除时，left指向的字符大于所需才能删除
+ * 同时，不再是覆盖了才能删除，而是添加完就能删除
+ * 例如t="ABCD"，s="AXXXXBCAXD"，在s遇到第二个A时，就可以把left挪到B
+*/
+class Solution {
+public:
     string minWindow(string s, string t) {
-
-        int hs[123],ht[123];
+        int hs[123], ht[123];
         int count = 0;
         string res;
-        for(auto c:t) ht[c] ++;
+        for (auto c : t) ht[c] ++;
 
-        for(int right = 0, left = 0; right < s.size(); right++){
-            hs[s[right]] ++;
-            if(hs[s[right]] <= ht[s[right]]) count ++;
-            不是看子串是否覆盖t，而是看left所指向字母的个数是否大于所需
-            while(hs[s[left]] > ht[s[left]]) {
+        for (int right = 0, left = 0; right < s.size(); right++){
+            hs[s[right]]++;
+            if (hs[s[right]] <= ht[s[right]]) count++;
+            //* 不是看子串是否覆盖t，而是看left所指向字母的个数是否大于所需
+            while (hs[s[left]] > ht[s[left]]) {
                 hs[s[left]]--;
                 left++;
             }
-            if(count == t.size()){
-                if(res.empty() || right - left + 1 < res.size()){
+            if (count == t.size()) {
+                if (res.empty() || right - left + 1 < res.size()) {
                     res = s.substr(left, right - left + 1);
                 }
             }
@@ -98,8 +105,6 @@ public:
         return res;
 
     }
-    */
-
 };
 // @lc code=end
 
